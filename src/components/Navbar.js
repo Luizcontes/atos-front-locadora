9b2535ebbc5bf2b165dac91bce9ebd7de32d655b
luiz <luiz.contes@hotmail.com>
2022-12-16 13:59:33 +0000

    bare minimal created

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import './Navbar.css';
+
+function Navbar() {
+
+  return (
+    <>
+      <di className="teste">LOGO</di>
+      <ul className="menu">
+        <li>HOME</li>
+        <li>CONTRATOS</li>
+        <li>CADASTRO</li>
+        <li>LOGIN</li>
+      </ul>
+    </>
+  );
+}
+
+export default Navbar;
